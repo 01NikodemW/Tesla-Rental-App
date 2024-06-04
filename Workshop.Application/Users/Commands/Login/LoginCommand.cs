ccabcf2988d500ba05893f12160736ec732749d0
Nikodem Wójcik <01nikodemw@gmail.com>
2024-06-04 20:08:05 +0200

    feat: add login validation

--- Workshop.Application/Users/Commands/Login/LoginCommand.cs
+++ Workshop.Application/Users/Commands/Login/LoginCommand.cs
@@ -5,6 +5,6 @@ namespace Workshop.Application.Users.Commands.Login;
 
 public class LoginCommand : IRequest<LoginResponse>
 {
-    public string Email { get; set; } = default!;
-    public string Password { get; set; } = default!;
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
--- Workshop.Application/Users/Commands/RegisterUser/RegisterUserCommand.cs
+++ Workshop.Application/Users/Commands/RegisterUser/RegisterUserCommand.cs
@@ -4,10 +4,10 @@ namespace Workshop.Application.Users.Commands.RegisterUser;
 
 public class RegisterUserCommand : IRequest
 {
-    public string Email { get; set; } = default!;
-    public string Password { get; set; } = default!;
-    public string PasswordConfirmation { get; set; } = default!;
-    public string FirstName { get; set; } = default!;
-    public string LastName { get; set; } = default!;
-    public string CountryId { get; set; } = default!;
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string PasswordConfirmation { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string CountryId { get; set; }
 }
